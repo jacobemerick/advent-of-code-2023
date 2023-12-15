d2d3423e79e8dd899c0a55af3ea8e19df2847249
Jacob Emerick <jacob.emerick@quantumrhino.com>
2023-12-15 10:31:25 -0700

    Day 7, pt 2

--- 07/2.py
+++ 07/2.py
@@ -4,25 +4,33 @@ from itertools import groupby
 def getHandTypeStrength(hand):
   sortedHand = sorted([*hand])
   groupedHands = []
+  wildCardCount = 0
   for i, group in groupby(sortedHand):
-    groupedHands.append(list(group))
+    if i == 'J':
+      wildCardCount = len(list(group))
+    else:
+      groupedHands.append(list(group))
   sortedGroups = sorted(groupedHands, key=lambda x: len(x), reverse=True)
 
-  if (len(sortedGroups[0]) == 5):
+  # edge case of all wild cards, making list empty
+  if (wildCardCount == 5):
     return 0
-  if (len(sortedGroups[0]) == 4):
+  
+  if (len(sortedGroups[0]) + wildCardCount == 5):
+    return 0
+  if (len(sortedGroups[0]) + wildCardCount == 4):
     return 1
-  if (len(sortedGroups[0]) == 3 and len(sortedGroups[1]) == 2):
+  if (len(sortedGroups[0]) + wildCardCount == 3 and len(sortedGroups[1]) == 2):
     return 2
-  if (len(sortedGroups[0]) == 3):
+  if (len(sortedGroups[0]) + wildCardCount == 3):
     return 3
-  if (len(sortedGroups[0]) == 2 and len(sortedGroups[1]) == 2):
+  if (len(sortedGroups[0]) == 2 and len(sortedGroups[1]) + wildCardCount == 2):
     return 4
-  if (len(sortedGroups[0]) == 2):
+  if (len(sortedGroups[0]) + wildCardCount == 2):
     return 5
   return 6
 
-cardStrength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+cardStrength = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 def getHandStrength(hand, index):
   return cardStrength.index([*hand][index])
 
